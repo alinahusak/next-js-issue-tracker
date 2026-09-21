147f90e8ceedbdcef8a2f6992e2adeca0cf7d4a2
alinahusak <alina.husak.dev@gmail.com>
2026-09-21 20:43:06 +0300

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -46,6 +46,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setError("An error occurred while creating the issue.");
     } finally {
