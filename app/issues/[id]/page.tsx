c9785e77b4820b5bb04bdd398baa63946ca4f8e4
alinahusak <alina.husak.dev@gmail.com>
2026-09-21 19:13:26 +0300

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,41 +1,58 @@
-import prisma from '@/prisma/client';
-import React from 'react'
-import ReactMarkdown from 'react-markdown';
-import { notFound } from 'next/navigation';
-import delay from 'delay';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes/dist/cjs/components/index.js';
-import {IssueStatusBadge} from '@/app/components';
-
+import prisma from "@/prisma/client";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { notFound } from "next/navigation";
+import delay from "delay";
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Heading,
+  Link,
+  Text,
+} from "@radix-ui/themes";
+import { IssueStatusBadge } from "@/app/components";
+import {Pencil2Icon} from "@radix-ui/react-icons";
 interface Props {
-    params: {
-        id: string;
-    }
+  params: {
+    id: string;
+  };
 }
 const IssueDetailPage = async ({ params }: Props) => {
-    const issue= await prisma.issue.findUnique({
-        where: {
-            id: parseInt(params.id)
-        }
-    });
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
 
-    await delay(2000);
+  await delay(2000);
 
-    if (!issue) {
-      notFound();
-    }
+  if (!issue) {
+    notFound();
+  }
 
   return (
-    <div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
         <Heading>{issue.title}</Heading>
-    <Flex className="space-x-3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-    </Flex>
-     <Card className="prose" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-     </Card>
-    </div>
-  )
-}
+        <Flex className="space-x-3" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit issue</Link>
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
